d94ab2a279cd09299b8b9326ad2730c950e42654
Noah <57117470+nolalito12345@users.noreply.github.com>
2025-11-29 18:58:48 -0500

    stuff & stuff

--- BankUml-main/bank/Main.java
+++ BankUml-main/bank/Main.java
@@ -4,7 +4,6 @@ import javax.swing.SwingUtilities;
 
 public class Main {
     public static void main(String[] args) {
-        // Launch GUI on the Event Dispatch Thread
         SwingUtilities.invokeLater(() -> {
             new BankingSystemGUI();
         });
